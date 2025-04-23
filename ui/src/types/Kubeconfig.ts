dad213b0f7ccb518497d8a534e8aebe595d8a9df
Yann Lacroix <yann.lacroix@advans-group.com>
2025-04-23 16:28:48 +0200

    🏷️ improve kubeconfig type

--- ui/src/types/Kubeconfig.ts
+++ ui/src/types/Kubeconfig.ts
@@ -1,4 +1,38 @@
 export interface Kubeconfig {
   name: string
-  kubeconfig: object
+  kubeconfig: {
+    apiVersion: string
+    kind: string
+    clusters: {
+      name: string
+      cluster: {
+        server: string
+        'certificate-authority-data'?: string
+        'insecure-skip-tls-verify'?: boolean
+      }
+    }[]
+    contexts?: {
+      name: string
+      context: {
+        cluster: string
+        user: string
+      }
+    }[]
+    'current-context'?: string
+    users?: {
+      name: string
+      user: {
+        'auth-provider': {
+          name: string
+          config: {
+            'client-id': string
+            'client-secret': string
+            'id-token': string
+            'idp-issuer-url': string
+            'refresh-token': string
+          }
+        }
+      }
+    }[]
+  }
 }
